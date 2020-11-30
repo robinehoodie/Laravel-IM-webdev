42e9fda74b93f7205b63e39efb133e94ee725224
robinehoodie <74012464+robinehoodie@users.noreply.github.com>
2020-11-30 22:38:48 +0800

    adf

---
+++ resources/views/Base/about.blade.php
@@ -0,0 +1,13 @@
+@extends('Base/layout')
+
+@section('body')
+        <div id="main" class="fade-in1">
+            <center>
+            <p style="font-size: 24px; line-height:0px; margin-bottom: 0px;">Tony's Lumber and Construction Supply </p>
+            <p style="font-size: 96px; line-height:100px; margin-top: 30px;margin-bottom: 0px;">TIMBER TRACK</p>
+            <p style="font-size: 18px; line-height:25px; margin-top: 10px;margin-bottom: 0px;">An inventory management system that allows a company to  manage <br>
+            supplies and keeps track  of the finance and progress of a work</p>
+            </center>
+        </div>
+ 
+@endsection
